ad0ae1257c35b55a2decd87612899d23ba86b0bb
Aytug Sevgi <33103753+aytugsevgi@users.noreply.github.com>
2021-04-25 20:31:17 +0300

    Update main.py

--- .script/main.py
+++ .script/main.py
@@ -1,25 +1,35 @@
 from subprocess import PIPE, run
 import os
 import subprocess
-# a = os.system('cd ~/Exercism/swift; for d in ./*/ ; do (cd "$d" && swift test && cd ..); done')
+
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
     return result.stdout
 
-#b = subprocess.check_output("cd {} ;ls".format(initPath), shell=True)
 b = out("ls")
-c = str(b).split('\n')
-c.remove('')
+folderList = str(b).split('\n')
+folderList.remove('')
 reportList = []
-for folder in c:
-    x = subprocess.check_output("cd {} && swift test".format(folder), shell=True, stderr=subprocess.STDOUT)
-    output = x.decode("utf-8")
-    translator = str.maketrans({chr(9): ''})
-    output = output.translate(translator)
-    arr = output.split("\n")
-    if "" in arr: 
-        arr.remove("")
-    reportList.append(arr[-1])
+onErrorList = []
+for folder in folderList:
+    try:
+        x = subprocess.check_output("cd {} && swift test".format(folder), shell=True, stderr=subprocess.STDOUT)
+        output = x.decode("utf-8")
+        translator = str.maketrans({chr(9): ''})
+        output = output.translate(translator)
+        arr = output.split("\n")
+        if "" in arr: 
+            arr.remove("")
+        reportList.append(arr[-1])
+    except subprocess.CalledProcessError as e:
+        output = e.output.decode("utf-8")
+        translator = str.maketrans({chr(9): ''})
+        output = output.translate(translator)
+        arr = output.split("\n")
+        if "" in arr: 
+            arr.remove("")
+        reportList.append(arr[-1])
+        onErrorList.append(folder)
 totalTest = 0
 totalFail = 0
 for report in reportList:
@@ -29,4 +39,11 @@ for report in reportList:
     totalFail += int(reportInfo[4])
 success = totalTest - totalFail
 average = ( success / totalTest ) * 100
+print("Total package count: {}".format(len(folderList)))
+print("Total test case: {}".format(totalTest))
+print("Total failed test case: {}".format(totalFail))
+if totalFail>0:
+    print("\tPackages that fail the test!")
+    print("\t{}\n".format(onErrorList))
 print("Test Successful Rate: {:.2f}%".format(average))
+
