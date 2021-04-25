ca920d3899981f20b5c81d2f63a845375577bb6a
Aytug Sevgi <33103753+aytugsevgi@users.noreply.github.com>
2021-04-25 23:35:20 +0300

    Update main.py

--- .script/main.py
+++ .script/main.py
@@ -5,12 +5,13 @@ import subprocess
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
     return result.stdout
-
+#base = "~/Exercism/swift/" - kendi localimizde denersek base path ile ilerleyelim
 b = out("ls")
 folderList = str(b).split('\n')
 folderList.remove('')
 reportList = []
-onErrorList = []
+testFaileds = []
+compilingError = []
 for folder in folderList:
     try:
         x = subprocess.check_output("cd {} && swift test".format(folder), shell=True, stderr=subprocess.STDOUT)
@@ -28,8 +29,14 @@ for folder in folderList:
         arr = output.split("\n")
         if "" in arr: 
             arr.remove("")
-        reportList.append(arr[-1])
-        onErrorList.append(folder)
+        if "Compiling" in arr[0]:
+            compilingError.append(folder)
+        elif "root manifest not found" in output:
+            continue
+        else:
+            reportList.append(arr[-1])
+            testFaileds.append(folder)
+
 totalTest = 0
 totalFail = 0
 for report in reportList:
@@ -40,9 +47,13 @@ for report in reportList:
 success = totalTest - totalFail
 average = ( success / totalTest ) * 100
 print("Total package count: {}".format(len(folderList)))
+if len(compilingError)>0:
+    print("Total compiling error: {}".format(len(compilingError)))
+    print("\t-Packages that fail on compile! (These packages are not included in the test rate)")
+    print("\t{}\n".format(compilingError))
 print("Total test case: {}".format(totalTest))
 print("Total failed test case: {}".format(totalFail))
 if totalFail>0:
-    print("\tPackages that fail the test!")
-    print("\t{}\n".format(onErrorList))
+    print("\t-Packages that fail the test!")
+    print("\t{}\n".format(testFaileds))
 print("Test Successful Rate: {:.2f}%".format(average))
