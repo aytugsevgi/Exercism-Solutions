a10d52de74ad75a57ac18db61f8901dea3f836fb
aytug.sevgi <aytug3667@gmail.com>
2021-04-26 12:03:02 +0300

    test

--- .script/main.py
+++ .script/main.py
@@ -8,6 +8,7 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
     ENDC = '\033[0m'
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
@@ -61,16 +62,16 @@ def runTests(path):
     # displaying the results on terminal
     success = totalTest - totalFail
     average = ( success / totalTest ) * 100
-    print(bcolors.BOLD + "Total package count: {}".format(len(folderList)) + bcolors.ENDC)
+    print(bcolors.UNDERLINE + "Total package count: {}".format(len(folderList)) + bcolors.ENDC)
+    print(bcolors.UNDERLINE + "Total test case: {}".format(totalTest) + bcolors.ENDC)
     if len(compilingError)>0:
         print(bcolors.FAIL + "Total compiling error: {}".format(len(compilingError)) + bcolors.ENDC)
         print(bcolors.FAIL + "\t-Packages that fail on compile! (These packages are not included in the test rate)" + bcolors.ENDC)
-        print("\t{}\n".format(compilingError))
-    print(bcolors.BOLD + "Total test case: {}".format(totalTest) + bcolors.ENDC)
+        print(bcolors.FAIL + "\t{}\n".format(compilingError) + bcolors.ENDC)
     print(bcolors.WARNING + "Total failed test case: {}".format(totalFail) + bcolors.ENDC)
     if totalFail>0:
         print(bcolors.WARNING + "\t-Packages that fail the test!" + bcolors.ENDC)
-        print("\t{}\n".format(testFaileds))
+        print(bcolors.WARNING + "\t{}\n".format(testFaileds) + bcolors.ENDC)
     rateColor = bcolors.OKGREEN
     if average<80.0 and average>=70.0:
         rateColor = bcolors.WARNING
