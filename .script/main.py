a970464dc8e25722ae9c924e85574c7ca2687222
Aytug Sevgi <33103753+aytugsevgi@users.noreply.github.com>
2021-05-29 12:09:27 +0300

    Update main.py

--- .script/main.py
+++ .script/main.py
@@ -35,7 +35,7 @@ def runTests(path):
             arr = output.split("\n")
             if "" in arr: 
                 arr.remove("")
-            print(arr)
+            print(arr[-3:])
             reportList.append(arr[-1])
             packageCount += 1
         except subprocess.CalledProcessError as e:
